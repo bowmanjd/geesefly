3cc90a0356b7b2b6af029af5001ad6537b7d7fd5
bowmanjd <bowmanjd@a7701288-7d19-ce46-f4f0-0e183cc38736>
2010-10-18 20:14:43 +0000

    separated endian checks/functionality

---
+++ skein_endian.h
@@ -0,0 +1,53 @@
+#ifndef _SKEIN_ENDIAN_H_
+#define _SKEIN_ENDIAN_H_     1
+
+#include <stdint.h>
+#include <sys/param.h>
+
+#ifndef BIGENDIAN
+# ifdef BYTE_ORDER
+#  if BYTE_ORDER == BIG_ENDIAN
+#   define BIGENDIAN
+#  endif
+# endif
+#endif
+
+#ifdef BIGENDIAN
+uint64_t ByteSwap64(uint64_t words) {
+    return  (( words       & 0xFF) << 56) |
+            (((words >> 8) & 0xFF) << 48) |
+            (((words >>16) & 0xFF) << 40) |
+            (((words >>24) & 0xFF) << 32) |
+            (((words >>32) & 0xFF) << 24) |
+            (((words >>40) & 0xFF) << 16) |
+            (((words >>48) & 0xFF) <<  8) |
+            (((words >>56) & 0xFF)      ) ;
+}
+
+void     words2bytes(uint8_t *dst,const uint64_t *src, uint16_t length) {
+    uint16_t n;
+
+    for (n=0;n<length;n++)
+        dst[n] = (uint8_t) (src[n>>3] >> (8*(n&7)));
+}
+
+void     bytes2words(uint64_t *dst,const uint8_t *src, uint16_t length) {
+    uint16_t n;
+
+    for (n=0;n<8*length;n+=8)
+        dst[n/8] = (((uint64_t) src[n  ])      ) +
+                   (((uint64_t) src[n+1]) <<  8) +
+                   (((uint64_t) src[n+2]) << 16) +
+                   (((uint64_t) src[n+3]) << 24) +
+                   (((uint64_t) src[n+4]) << 32) +
+                   (((uint64_t) src[n+5]) << 40) +
+                   (((uint64_t) src[n+6]) << 48) +
+                   (((uint64_t) src[n+7]) << 56) ;
+    }
+#else
+#define ByteSwap64(words)  (words)
+#define words2bytes(dst08,src64,bCnt) memcpy(dst08,src64,bCnt)
+#define bytes2words(dst64,src08,wCnt) memcpy(dst64,src08,8*(wCnt))
+#endif
+
+#endif  /* ifndef _SKEIN_ENDIAN_H_ */
